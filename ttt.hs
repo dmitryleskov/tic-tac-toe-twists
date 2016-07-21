ce0c01ca93ebde7853a13b0687bd84a0f1624963
Dmitry Leskov <github@dmitryleskov.com>
2016-07-21 09:55:22 +0600

    Store blanks in Field, get rid of availableMoves

--- ttt.hs
+++ ttt.hs
@@ -10,7 +10,8 @@ instance Show Cell where
     show Blank  = "."
 
 data Field = Field { crosses :: Set.Set Int,
-                     noughts :: Set.Set Int }
+                     noughts :: Set.Set Int,
+                     blanks :: Set.Set Int }
                      
 instance Show Field where
     show field = concat $ intersperse "\n" lines
@@ -20,20 +21,18 @@ instance Show Field where
           | Set.member n (crosses field) = show Cross
           | Set.member n (noughts field) = show Nought
           | otherwise = show Blank
-                     
-
-availableMoves :: Field -> Set.Set Int
-availableMoves field = Set.fromList [1,2,3,4,5,6,7,8,9] \\ (crosses field) \\ (noughts field)
 
 move :: Field -> Int -> Cell -> Field    
-move field n Cross = field { crosses = Set.insert n (crosses field) }
-move field n Nought = field { noughts = Set.insert n (noughts field) }
+move field n Cross = field { crosses = Set.insert n (crosses field), blanks = Set.delete n (blanks field) }
+move field n Nought = field { noughts = Set.insert n (noughts field), blanks = Set.delete n (blanks field) }
 
-initField = Field { crosses = Set.empty, noughts = Set.empty }
+initField = Field { crosses = Set.empty, noughts = Set.empty, blanks = Set.fromList [1..9] }
+{--
 someField = Field { crosses = Set.fromList [1,4,5], noughts = Set.fromList [2,9] }
 testField1 = Field { crosses = Set.fromList [1], noughts = Set.fromList [5] }
 testField2 = Field { crosses = Set.fromList [2,8], noughts = Set.fromList [1] }
 testField3 = Field { crosses = Set.fromList [1,2,7], noughts = Set.fromList [3,4,5] }
+--}
 
 streaks :: [Set.Set Int]
 streaks = [Set.fromList [1,2,3],
@@ -56,11 +55,11 @@ minimax :: Field -> Cell -> (Int, Maybe Int)
 minimax field movesNext =
     case gameOver field of
     Just Blank -> (0, Nothing)
-    Just Cross -> (1 + length (availableMoves field), Nothing)
-    Just Nought -> (-1 - length (availableMoves field), Nothing)
+    Just Cross -> (1 + length (blanks field), Nothing)
+    Just Nought -> (-1 - length (blanks field), Nothing)
     _ -> if movesNext == Cross
-             then foldr maximize (minBound :: Int, Nothing) $ availableMoves field
-             else foldr minimize (maxBound :: Int, Nothing) $ availableMoves field 
+             then foldr maximize (minBound :: Int, Nothing) $ blanks field
+             else foldr minimize (maxBound :: Int, Nothing) $ blanks field 
                 where
                   maximize n old@(bestScore, _) =
                       let (score, _) = minimax (move field n Cross) Nought
@@ -72,12 +71,11 @@ minimax field movesNext =
                       in  if score < bestScore
                               then (score, Just n)
                               else old
-                           
                         
 step :: Field -> Int -> Field
-step field count =
+step field _ =
     traceShow field $
-    let player = if count `rem` 2 == 1
+    let player = if odd $ Set.size $ blanks field
                      then Cross
                      else Nought
         (score, m) = minimax field player
@@ -89,11 +87,11 @@ main = do
 --    print testField3
   --  print $ availableMoves testField3
     print $ minimax initField Cross
-    print $ minimax someField Nought
-    print $ minimax testField1 Cross
-    print $ minimax testField2 Nought
-    print $ minimax testField3 Cross
+--    print $ minimax someField Nought
+--    print $ minimax testField1 Cross
+--    print $ minimax testField2 Nought
+--    print $ minimax testField3 Cross
     
     print $ foldl step initField [1..9]
-    print $ foldl step testField2 [4..9]
+--    print $ foldl step testField2 [4..9]
 
