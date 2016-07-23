6bccbf21b38c2948a016f61a549a36b6e86cc958
Dmitry Leskov <github@dmitryleskov.com>
2016-07-23 22:29:36 +0600

    Optimize scanning (add prescan, reduce number of frames)

--- Growing.hs
+++ Growing.hs
@@ -25,11 +25,12 @@ updateCount cell delta frame =
                     
 -- Numbers of Cross and Nought moves to this Frame to win                     
 ranks :: Frame -> (Maybe Int, Maybe Int)
-ranks f = case f of
-             Frame{noughts = 0, crosses = 0} -> (Just rank, Just rank)
-             Frame{noughts = 0} -> (Just rank, Nothing)
-             Frame{crosses = 0} -> (Nothing, Just rank)
-             _ -> (Nothing, Nothing)
+ranks f = 
+    case f of
+        Frame{noughts = 0, crosses = 0} -> (Just rank, Just rank)
+        Frame{noughts = 0} -> (Just rank, Nothing)
+        Frame{crosses = 0} -> (Nothing, Just rank)
+        _ -> (Nothing, Nothing)
   where rank = (blanks f) + (neighbours f) * 2 + (remotes f) * 5
 
 bestRanks :: (Maybe Int, Maybe Int) -> (Maybe Int, Maybe Int) -> (Maybe Int, Maybe Int)
@@ -40,66 +41,75 @@ bestRanks (c1, n1) (c2, n2) = (best c1 c2, best n1 n2)
     best (Just r1) (Just r2) = Just $ min r1 r2
 
 scan :: Field -> (Int, Int) -> (Int, Int) -> Int -> (Maybe Int, Maybe Int)
-scan field start step@(dx, dy) count = 
-    let frame = Frame { crosses = 0, noughts = 0, blanks = 0, neighbours = 0 }
-    in  loop (Nothing, Nothing) start count frame 
+scan field start@(x, y) step@(dx, dy) count = 
+    let frame = preScan win
+    in  loop (ranks frame) start count frame 
   where
+    preScan :: Int -> Frame
+    preScan count =
+        preScanLoop Frame { crosses = 0, noughts = 0, blanks = 0, neighbours = 0 } $ count
+      where
+        preScanLoop :: Frame -> Int -> Frame
+        preScanLoop frame 0 = frame
+        preScanLoop frame count =
+            let newFrame = updateCount (cell (x + dx * (count - 1), y + dy * (count - 1)) field) 1 frame
+            in  preScanLoop newFrame (count - 1)
     loop :: (Maybe Int, Maybe Int) -> (Int, Int) -> Int -> Frame -> (Maybe Int, Maybe Int)
     loop best _ 0 frame = {--trace ((show frame) ++ "\n") $ --} best
     loop best (x, y) count frame =
 --        trace (show frame) $
-        let newXY = (x + dx, y + dy)
-            trailXY = (x - dx * (win-1), y - dy * (win-1))
+        let newXY = (x + dx * win, y + dy * win)
+            trailXY = (x, y)
             add = cell newXY field
             drop = cell trailXY field
             newFrame = updateCount drop (-1) $
                        updateCount add 1 $
                        frame 
             newBest = bestRanks best $ ranks newFrame
-        in  loop newBest newXY (count-1) newFrame
+        in  loop newBest (x + dx, y + dy) (count-1) newFrame
             
-data ScanRule = ScanRule { start :: Boundaries -> (Int, Int),
-                           outerStep :: (Int, Int),
-                           outerCount :: Boundaries -> Int,
-                           innerStep :: (Int, Int),
-                           innerCount :: Boundaries -> Int }
+data ScanRule = ScanRule { start :: Boundaries -> (Int, Int), -- Starting cell
+                           outerStep :: (Int, Int),           -- Scan rows offset
+                           outerCount :: Boundaries -> Int,   -- Number of scan rows
+                           innerStep :: (Int, Int),           -- Frame offset
+                           innerCount :: Boundaries -> Int }  -- Number of scan frames
 
 scanRules = [-- Rows
-             ScanRule { start = \b -> (minX b - 2, minY b),
+             ScanRule { start = \b -> (minX b, minY b),
                         outerStep = (0, 1),
                         outerCount = \b -> height b,
                         innerStep = (1, 0),
-                        innerCount = \b -> width b + 2},
+                        innerCount = \b -> width b - win},
              -- Columns       
-             ScanRule { start = \b -> (minX b, minY b - 2),
+             ScanRule { start = \b -> (minX b, minY b),
                         outerStep = (1, 0),
                         outerCount = \b -> width b,
                         innerStep = (0, 1),
-                        innerCount = \b -> height b + 2},
+                        innerCount = \b -> height b - win},
              -- Main diagonal and diagonals below it
-             ScanRule { start = \b -> (minX b - 2, minY b - 2),
+             ScanRule { start = \b -> (minX b, minY b),
                         outerStep = (1, 0),
-                        outerCount = \b -> (width b - win + 1),
+                        outerCount = \b -> (width b - win),
                         innerStep = (1, 1),
-                        innerCount = \b -> height b + 2},
+                        innerCount = \b -> height b - win},
              -- Diagonals above main
-             ScanRule { start = \b -> (minX b - 2, minY b - 1),
+             ScanRule { start = \b -> (minX b, minY b + 1),
                         outerStep = (0, 1),
-                        outerCount = \b -> (height b - win),
+                        outerCount = \b -> (height b - win - 1),
                         innerStep = (1, 1),
-                        innerCount = \b -> width b + 2},
+                        innerCount = \b -> width b - win},
              -- Antidiagonal and diagonals above it
-             ScanRule { start = \b -> (minX b - 2, maxY b + 2),
+             ScanRule { start = \b -> (minX b, maxY b),
                         outerStep = (1, 0),
-                        outerCount = \b -> (width b - win + 1),
+                        outerCount = \b -> (width b - win),
                         innerStep = (1, -1),
-                        innerCount = \b -> height b + 2},
+                        innerCount = \b -> height b - win},
              -- Diagonals below the antidiagonal           
-             ScanRule { start = \b -> (minX b - 2, maxY b + 1),
+             ScanRule { start = \b -> (minX b, maxY b -1),
                         outerStep = (0, -1),
-                        outerCount = \b -> (height b - win),
+                        outerCount = \b -> (height b - win - 1),
                         innerStep = (1, -1),
-                        innerCount = \b -> height b + 2}
+                        innerCount = \b -> height b - win}
             ]
             
 uniScan field rule =
